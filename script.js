bde30fde8d30cd8edea143ad20ace3e868dcbd57
Jacob Jensen <jaj.jacob.jensen@gmail.com>
2021-02-22 14:35:58 -0600

    first draft 6

--- script.js
+++ script.js
@@ -24,3 +24,29 @@ function closePopup(popup) {
     if (popup == null) return
     popup.classList.remove('popup_opened')
 }
+
+// Let's find the form in the DOM
+let formElement = // Use the querySelector() method
+
+    // Next is the form submit handler, though
+    // it won't submit anywhere just yet
+    function handleFormSubmit(evt) {
+        evt.preventDefault(); // This line stops the browser from submitting the form in the default way.
+        // Having done so, we can define our own way of submitting the form.
+        // We'll explain it in more detail later.
+
+        // Let's find the form fields in the DOMe
+        let nameInput = // Use querySelector()
+            let jobInput = // Use querySelector()
+
+
+                // Get the values of each field from the corresponding value property
+
+                // Select elements where the field values will be entered
+
+                // Insert new values using the textContent property of the querySelector() method
+    }
+
+// Connect the handler to the form:
+// it will watch the submit event
+formElement.addEventListener('submit', handleFormSubmit);
