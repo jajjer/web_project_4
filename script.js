33fe23321657f661a95af7f6e06cd342e03c735b
Jacob Jensen <jaj.jacob.jensen@gmail.com>
2021-02-22 19:46:26 -0600

    first draft 7

--- script.js
+++ script.js
@@ -1,52 +1,7 @@
-const openPopupButtons = document.querySelectorAll('[data-popup-target]')
-const closePopupButtons = document.querySelectorAll('[data-close-button]')
+const profile__button = document.querySelector(".profile__button");
+const popup_opened = document.querySelector(".popup_opened");
+const popup__close = document.querySelector(".popup__close");
+const popup = document.querySelector(".popup");
 
-openPopupButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const popup = document.querySelector(button.popupTarget)
-        openPopup(popup)
-    })
-})
-
-closePopupButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const popup = button.closest('.popup')
-        closePopup(popup)
-    })
-})
-
-function openPopup(popup) {
-    if (popup == null) return
-    popup.classList.add('popup_opened')
-}
-
-function closePopup(popup) {
-    if (popup == null) return
-    popup.classList.remove('popup_opened')
-}
-
-// Let's find the form in the DOM
-let formElement = // Use the querySelector() method
-
-    // Next is the form submit handler, though
-    // it won't submit anywhere just yet
-    function handleFormSubmit(evt) {
-        evt.preventDefault(); // This line stops the browser from submitting the form in the default way.
-        // Having done so, we can define our own way of submitting the form.
-        // We'll explain it in more detail later.
-
-        // Let's find the form fields in the DOMe
-        let nameInput = // Use querySelector()
-            let jobInput = // Use querySelector()
-
-
-                // Get the values of each field from the corresponding value property
-
-                // Select elements where the field values will be entered
-
-                // Insert new values using the textContent property of the querySelector() method
-    }
-
-// Connect the handler to the form:
-// it will watch the submit event
-formElement.addEventListener('submit', handleFormSubmit);
+profile__button.addEventListener("click", function() { popup_opened.style.display = "block" });
+profile__close.addEventListener("click", function() { popup_opened.display = "none" });
