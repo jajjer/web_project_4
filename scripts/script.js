0bf68f29ead6d4343028f2e0651e1290612e581f
Jacob Jensen <jaj.jacob.jensen@gmail.com>
2021-02-27 19:09:08 -0600

    first draft 14

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,7 @@ const popupOpened = document.querySelector(".popup_opened");
 const popupClose = document.querySelector(".popup__close");
 const popup = document.querySelector(".popup");
 const popupSave = document.querySelector(".popup__save");
-const formElement = document.querySelector('.popup__form');
+
 
 function openPopup() {
     popup.classList.add('popup_opened')
@@ -18,16 +18,21 @@ function savePopup() {
 
 }
 
+
+const formElement = document.querySelector('.popup__form');
+
 function handleFormSubmit(evt) {
     evt.preventDefault();
     let nameInput = document.querySelector('popup__profile_name').value;
     let jobInput = document.querySelector('popup__profile_about').value;
+    //let profileName = document.querySelector('profile__title')
 
-    document.querySelector('profile__title').value = nameInput.textContent;
-    document.querySelector('profile__subtitle').value = jobInput.textContent;
+
+    nameInput.textContent = document.querySelector('profile__title');
+    document.querySelector('profile__subtitle') = jobInput.textContent;
 }
 
 profileButton.addEventListener("click", openPopup);
 popupClose.addEventListener("click", closePopup);
 popupSave.addEventListener("click", closePopup);
-formElement.addEventListener('submit', handleFormSubmit);
+formElement.addEventListener("submit", handleFormSubmit);
