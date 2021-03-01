33105c43daec14e46cda7fc3ab1797dfb7ae18da
Jacob Jensen <jaj.jacob.jensen@gmail.com>
2021-02-28 20:48:28 -0600

    first draft 17

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,12 @@ const profileButton = document.querySelector(".profile__button");
 const popupOpened = document.querySelector(".popup_opened");
 const popupClose = document.querySelector(".popup__close");
 const popup = document.querySelector(".popup");
-const popupSave = document.querySelector(".popup__save");
-const formElement = document.querySelector(".popup__form");
+const popupSave = document.querySelector(".form__save");
+const formElement = document.querySelector(".form");
+const nameInput = document.querySelector(".form__input_name");
+const jobInput = document.querySelector(".form__input_about");
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
 
 
 function openPopup() {
@@ -14,20 +18,14 @@ function closePopup() {
     popup.classList.remove('popup_opened')
 }
 
-function savePopup() {
-    popupSave.querySelector('popup__profile_name').value;
-
-}
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
-    let nameInput = document.querySelector(".popup__profile_name").value;
-    let jobInput = document.querySelector(".popup__profile_about").value;
-    document.querySelector(".profile__title").textContent = nameInput;
-    document.querySelector(".profile__subtitle").textContent = jobInput;
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
+    closePopup();
 }
 
 profileButton.addEventListener("click", openPopup);
 popupClose.addEventListener("click", closePopup);
-popupSave.addEventListener("click", closePopup);
 formElement.addEventListener("submit", handleFormSubmit);
