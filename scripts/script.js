fe6fab2ff09d76b60ff46be5b226087c4f00bbdb
Jacob Jensen <jaj.jacob.jensen@gmail.com>
2021-04-13 16:04:28 -0500

    feature/add photos

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,66 @@ const jobInput = document.querySelector(".form__input_type_about");
 const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
 
+const add = document.querySelector(".add");
+const addOpened = document.querySelector(".add_opened");
+const addClose = document.querySelector(".add__close");
+const addButton = document.querySelector(".profile__add-button");
+const titleInput = document.querySelector(".form__input_type_title");
+const imageInput = document.querySelector(".form__input_type_image");
+const likeButton = document.querySelector(".element__button");
+const likeButtonActive = document.querySelector(".element__button_active");
+const elementTitle = document.querySelector(".element__title");
+const elementImage = document.querySelector(".element__image");
+const template = document.querySelector("#template__photo").content;
+const newElement = template.querySelector(".element__item").cloneNode(true);
+const templateList = document.querySelector(".template__list");
+
+const initialCards = [{
+        name: "Yosemite Valley",
+        link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    },
+    {
+        name: "Lake Louise",
+        link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    },
+    {
+        name: "Bald Mountains",
+        link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    },
+    {
+        name: "Latemar",
+        link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    },
+    {
+        name: "Vanoise National Park",
+        link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+    },
+    {
+        name: "Lago di Braies",
+        link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    }
+];
+
+initialCards.forEach(element => console.log(element.name = elementTitle));
+initialCards.forEach(element => console.log(element.link = elementImage));
+
+
+newElement.querySelector(".element__image").src =
+    templateList.append(newElement);
+
+
+function openAdd() {
+    add.classList.add('add_opened')
+}
+
+function closeAdd() {
+    add.classList.remove('add_opened')
+}
+
+function like() {
+    likeButton.classList.add('element__button_active')
+    likeButton.classList.remove('element__button')
+}
 
 function openPopup() {
     popup.classList.add('popup_opened')
@@ -31,3 +91,6 @@ function handleFormSubmit(evt) {
 profileButton.addEventListener("click", openPopup);
 popupClose.addEventListener("click", closePopup);
 formElement.addEventListener("submit", handleFormSubmit);
+addButton.addEventListener("click", openAdd);
+addClose.addEventListener("click", closeAdd);
+likeButton.addEventListener("click", like);
