668df1ea32763dce6bf5846bd5cb2893de18101e
Jacob Jensen <jaj.jacob.jensen@gmail.com>
2021-05-30 12:45:29 -0500

    hotfix/ idk

--- scripts/script.js
+++ scripts/script.js
@@ -10,15 +10,114 @@ const jobInput = document.querySelector(".popup__input_type_description");
 const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
 
+const popupNewCard = document.querySelector(".popup_type_new-card");
+const addButton = document.querySelector(".profile__add-button");
+const titleInput = document.querySelector(".popup__input_type_card-name");
+const imageInput = document.querySelector(".popup__input_type_url");
+const popupImage = document.querySelector(".popup__content_content_image");
 
-function openPopup() {
-    popupEdit.classList.add('popup_opened')
-    nameInput.value = profileTitle.textContent;
-    jobInput.value = profileSubtitle.textContent;
+
+
+const closeEditButton = popupEdit.querySelector('.popup__close');
+const closeNewCardButton = popupNewCard.querySelector('.popup__close');
+const closeImageButton = popupImage.querySelector('.popup__close');
+
+
+
+const likeButtonActive = document.querySelector(".element__button_active");
+const elementTitle = document.querySelector(".element__title");
+const elementImage = document.querySelector(".element__image");
+const elementList = document.querySelector(".element");
+
+const initialCards = [{
+        name: "Yosemite Valley",
+        link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    },
+    {
+        name: "Lake Louise",
+        link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    },
+    {
+        name: "Bald Mountains",
+        link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    },
+    {
+        name: "Latemar",
+        link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    },
+    {
+        name: "Vanoise National Park",
+        link: "https://code.s3.yandex.net/web-code/vanoise.jpg"
+    },
+    {
+        name: "Lago di Braies",
+        link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    }
+];
+
+function createCard(card) {
+    const cardTemplate = document.querySelector("#template__photo").content.querySelector(".element__item");
+
+    const cardElement = cardTemplate.cloneNode(true);
+    const cardImage = cardElement.querySelector(".element__image");
+    const cardTitle = cardElement.querySelector(".element__title");
+
+    const likeButton = cardElement.querySelector(".element__button");
+
+    const deleteButton = cardElement.querySelector(".element__trash");
+
+    const trashWrapper = cardElement.querySelector(".element__container");
+
+    cardImage.style.backgroundImage = `url(${card.link})`;
+    cardTitle.textContent = card.name;
+
+    cardImage.addEventListener('click', () => handlePreviewPicture(card));
+
+    likeButton.addEventListener("click", (e) =>
+        likeButton.classList.toggle("element__button_active")
+    );
+
+    deleteButton.addEventListener("click", (e) =>
+        trashWrapper.parentElement.remove()
+    );
+
+    return cardElement;
+};
+
+function renderCard(card, elementList) {
+    elementList.append(createCard(card));
+};
+
+initialCards.forEach(card => {
+    renderCard(card, elementList)
+});
+
+
+
+
+function addCard(evt) {
+    evt.preventDefault();
+    let newCard = { name: "", link: "" };
+    newCard.name = addTitle.value;
+    newCard.link = addImage.value;
+
+    initialCards.push(newCard);
+};
+
+
+
+
+
+function toggleEdit() {
+    popupEdit.classList.toggle("popup_opened")
 }
 
-function closePopup() {
-    popup.classList.remove('popup_opened')
+function toggleNewCard() {
+    popupNewCard.classList.toggle("popup_opened")
+}
+
+function toggleImage() {
+    popupImage.classList.toggle("popup_opened")
 }
 
 
@@ -26,9 +125,13 @@ function handleFormSubmit(evt) {
     evt.preventDefault();
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
-    closePopup();
+    toggleEdit();
 }
 
-profileButton.addEventListener("click", openPopup);
-popupClose.addEventListener("click", closePopup);
+profileButton.addEventListener("click", toggleEdit);
+closeEditButton.addEventListener("click", toggleEdit);
 formElement.addEventListener("submit", handleFormSubmit);
+addButton.addEventListener("click", toggleNewCard);
+closeNewCardButton.addEventListener("click", toggleNewCard);
+formElement.addEventListener("submit", addCard);
+elementImage.addEventListener("click", toggleImage);
